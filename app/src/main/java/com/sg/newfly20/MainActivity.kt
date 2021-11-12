2496232d365d5f763c9e84f754428b721baed54a
shlomo <shlomo.gevim@gmail.com>
2021-11-12 09:12:35 +0200

    12/11/2021 after Mr man Walking and add Model1

--- app/src/main/java/com/sg/newfly20/MainActivity.kt
+++ app/src/main/java/com/sg/newfly20/MainActivity.kt
@@ -5,10 +5,7 @@ import android.content.res.Resources
 import android.media.CamcorderProfile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.Gravity
-import android.view.MotionEvent
-import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.core.content.res.ResourcesCompat
@@ -23,26 +20,98 @@ import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import org.w3c.dom.Text
 import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
     lateinit var arFragment: ArFragment
 
-    private val model = Models.Fish               //model-201
+   /* private val model = Models.Fish               //model-201
     private val modelResourceId = R.raw.fish
     val animationString = "Armature|ArmatureAction"
-    private var spaScale = false
-    private var thisScale = .7f
-    val bubleX = 0.8f              //vertical
-    val bubleY = -0.7f
+     private var modelScale = 0.2f*/
+
+    /*private val model = Models.Book             //model-202
+    private val modelResourceId = R.raw.book
+    val animationString = "TEST2"
+    private var modelScale = 0.03f*/
+
+   /* private val model = Models.Wolf             //model-203
+    private val modelResourceId = R.raw.wolf
+    val animationString = "01_Run"
+    private var modelScale = 3f*/
+
+   /* private val model = Models.Mei_Run             //model-204
+    private val modelResourceId = R.raw.mei_run
+    val animationString = "Mei_Run"
+    private var modelScale = 3f*/
+
+   /* private val model = Models.BorisEntery        //model-205
+    private val modelResourceId = R.raw.entry
+    val animationString = "mixamo.com"
+    private val modelScale = 3f
+    private val startAngle=180f*/
+
+   /* private val model = Models.BorisSammba       //model-205A
+    private val modelResourceId = R.raw.samba
+    val animationString = "mixamo.com"
+    private val modelScale = 3f
+    private val startAngle=180f*/
+
+   /* private val model = Models.BorisHipHop          //model-205B
+    private val modelResourceId = R.raw.hip_hop_dancing
+    val animationString = "mixamo.com"
+    private val modelScale = 3f
+    private val startAngle=180f*/
+
+   /* private val model = Models.BorisSwing            //model-205C
+    private val modelResourceId = R.raw.swing_dancing
+    val animationString = "mixamo.com"
+    private val modelScale = 3f
+    private val startAngle=180f*/
+
+   /* private val model = Models.DancerGirl            //model-206
+    private val modelResourceId = R.raw.dancer_girl
+    val animationString = "mixamo.com"
+    private val modelScale = 0.1f
+    private val startAngle=180f
+*/
+
+   /* private val model = Models.Guppie           //model-207
+    private val modelResourceId = R.raw.guppie207
+    val animationString = "Guppie_Animated"
+    private val modelScale = 0.1f
+    private val startAngle=90f*/
+
+    /*private val model = Models1.Broaddtail           //model-208
+    private val modelResourceId = R.raw.broadtailmoor
+    val animationString = "Scene"
+    private val modelScale = 1.5f*/
+
+    private val model = Models1.MrManWalking          //model-209
+    private val modelResourceId = R.raw.mrmanwalking
+    val animationString = "mixamo.com"
+    private val modelScale = 0.1f
+
+    private var startAngle=270f    // pay attention to Models1 startAngle
+
+
+    val rotatePosition = true
+
+
+
+
+    val verticalDisplacementFactor = 1.0f              //vertical
+    val horizontalDisplacementFactor = -0.7f
+    val textviewWight = 2000
+    val textviewHight = 4000
+    val fontSize=150f
 
     private val nodes = mutableListOf<RotatingNode>()
     private lateinit var videoRecorder: VideoRecorder
     private var isRecording = false
     private fun getCurrentScene() = arFragment.arSceneView.scene
     val viewNodes = mutableListOf<Node>()
-    var selectBtn1 = false
+
     var text1 = ""
     var text2 = ""
     lateinit var textView1: TextView
@@ -54,6 +123,10 @@ class MainActivity : AppCompatActivity() {
     var viewNode_1: Node? = null
     var viewNode_2: Node? = null
 
+    var but1_On=false
+    var but2_On=false
+    var but3_On=false
+    var but4_On=false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,13 +138,25 @@ class MainActivity : AppCompatActivity() {
         getTextView()
         setUpCamera()
         setupButtons()
+        getCurrentScene().addOnUpdateListener {
+            rotateViewNodesTowardsUser()
+        }
+
+        if (rotatePosition){
+            startAngle=model.startAngelRotation
+        }else{
+            startAngle=model.startAngelStatic
+        }
+
+
     }
 
     private fun setupModel() {
         arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            anchorGlobal = hitResult.createAnchor()
             loadModel { modelRenderable, viewRendable1, viewRendable2 ->
                 addNodeToScene(
-                    hitResult.createAnchor(), modelRenderable, viewRendable1, viewRendable2
+                    anchorGlobal, modelRenderable, viewRendable1, viewRendable2
                 )
                 eliminateDot()
             }
@@ -109,7 +194,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         val anchorNode = AnchorNode(anchor)
         getCurrentScene().addChild(anchorNode)
-        anchorGlobal = anchor
+
 
         val rotatingNode = RotatingNode(model.degreesPerSecond).apply {
             setParent(anchorNode)
@@ -118,12 +203,15 @@ class MainActivity : AppCompatActivity() {
         val modelNode = Node().apply {
             renderable = modelRenewable
             localPosition = Vector3(model.radius, model.height, 0f)
-            localRotation = Quaternion.eulerAngles(Vector3(0f, model.rotationDegrees, 0f))
-            if (!spaScale) {
-                localScale = Vector3(thisScale, thisScale, thisScale)
+            localRotation = Quaternion.eulerAngles(Vector3(0f, model.rotationDegrees+startAngle, 0f))
+          //  if (!spaScale) {
+                localScale = Vector3(modelScale, modelScale, modelScale)
+         //   }
+            if (rotatePosition) {
+                setParent(rotatingNode)
+            } else {
+                setParent(anchorNode)
             }
-            //  setParent(rotatingNode)
-            setParent(anchorNode)
         }
         modelNodeGlobal = modelNode
 
@@ -140,38 +228,53 @@ class MainActivity : AppCompatActivity() {
 
     private fun addView(viewRenderable: ViewRenderable): Node {
         return Node().apply {
-            // renderable = null
             val box = modelNodeGlobal?.renderable?.collisionShape as Box
-            localPosition = Vector3(0f, box.size.y * bubleX, box.size.y * bubleY)
+            localPosition = Vector3(
+                0f,
+                box.size.y * verticalDisplacementFactor,
+                box.size.y * horizontalDisplacementFactor
+            )
             localRotation = Quaternion.eulerAngles(Vector3(0f, model.rotationDegrees + 90f, 0f))
-            if (!spaScale) {
-                localScale = Vector3(thisScale, thisScale, thisScale)
-            }
+           //if (!spaScale) {
+                localScale = Vector3(modelScale, modelScale, modelScale)
+          //  }
             renderable = viewRenderable
+        }
+    }
 
+    private fun rotateViewNodesTowardsUser() {
+        for (node in viewNodes) {
+            node.renderable?.let {
+                val camPos = getCurrentScene().camera.worldPosition
+                val viewNodePos = node.worldPosition
+                val dir = Vector3.subtract(camPos, viewNodePos)
+                node.worldRotation = Quaternion.lookRotation(dir, Vector3.up())
+            }
         }
     }
 
     private fun getTextView() {
-        text1 = "     לא מבין למה אתה מסתכל עלי" +
-                "\n" + "מה, אין לך מה לעשות" +
-                "\n" + "     זה קצת מטריד אותי המבטים שלך" +
-                "\n" + "קצת מטריד..."
+        text1 = "  תגיד למה אתה מסתכל עלי כל הזמן?" +
+                "\n" + "      ובכלל איפה הכיוון של הים? " +
+                "\n" + "      ב'ואנה , אני שוחה כאן כבר חצי שעה " +
+                "\n" + "   ונראה לי שאני שוחה די במעגל כל הזמן ..."+
+                "\n"+ "\n"
         textView1 = createTextView(text1)
-        text2 = "     עזוב אותי באמאשל'ך" +
-                "\n" + "עזוב אותי" +
-                "\n" + "     בינינו,איזה ברדק בעולם שלך" +
-                "\n" + "אני חוזר לים ..."
+        text2 =  "\n" +"       ומה זה כל הברדק הזה בחדר שלך ?" +
+                "\n" + "     בו'אנה תן לחזור לים הגדול" +
+                "\n" + "               שם הרבה יותר נעים לי..."+
+                "\n"+ "\n"
         textView2 = createTextView(text2)
     }
 
     private fun createTextView(text1: String): TextView {
         return TextView(this).apply {
-            setBackgroundResource(R.drawable.thinking)
-            setLayoutParams(ViewGroup.LayoutParams(300.toPx(), 300.toPx()))
+            setBackgroundResource(R.drawable.thiking10)
+            setLayoutParams(ViewGroup.LayoutParams(textviewHight.toPx(), textviewWight.toPx()))
             text = text1
-            setTextColor(android.graphics.Color.WHITE)
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+            setTextColor(android.graphics.Color.BLACK)
+           // setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize)
+            textSize=fontSize
             gravity = Gravity.CENTER
             typeface = ResourcesCompat.getFont(
                 this@MainActivity,
@@ -182,16 +285,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupButtons() {
         btn1.setOnClickListener {
-            viewNode_1?.setParent(modelNodeGlobal)
+            if (!but1_On){
+                viewNode_1?.setParent(modelNodeGlobal)
+                viewNode_1?.let { it -> viewNodes.add(it) }
+                but1_On=true
+            }else{
+                viewNode_1?.setParent(null)
+                but1_On=false
+            }
         }
-
         btn2.setOnClickListener {
-            viewNode_1?.setParent(null)
-            viewNode_2?.setParent(modelNodeGlobal)
-
+            if (!but2_On){
+                viewNode_2?.setParent(modelNodeGlobal)
+                viewNode_2?.let { it -> viewNodes.add(it) }
+                but2_On=true
+            }else{
+                viewNode_2?.setParent(null)
+                but2_On=false
+            }
         }
-
     }
+
 
     fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
 
@@ -213,6 +327,60 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
     private fun setupFab() {
         fab.setOnClickListener {
+                isRecording = videoRecorder.toggleRecordingState()
+                true
+            }
+
+        fab.setOnLongClickListener {
+                if ( isRecording) {
+                    isRecording = videoRecorder.toggleRecordingState()
+                    Toast.makeText(this, "Save video to gallery ... ", Toast.LENGTH_LONG).show()
+                    true
+                } else false
+            }
+
+    }
+
+    /*  private fun getTextView() {
+       text1 = "          תגיד למה אתה מסתכל עלי כל הזמן?" +
+               "\n" + "     מה לא ראית דג בחיים שלך ?" +
+               "\n" + "      ובכלל איפה הכיוון של הים? " +
+               "\n" + "        פעם אחרונה שאני משאיר את הוויז שלי בבית שלי " +
+               "\n" + "      ב'ואנה ,  אני שוחה כאן כבר חצי שעה " +
+               "\n" + "   ונראה לי שאני שוחה די במעגל כל הזמן ..."+
+               "\n"+ "\n"
+       textView1 = createTextView(text1)
+       text2 = "                 בכלל מה זה המקום הזה ?" +
+               "\n" + "                          ומי זה כל האנשים העצובים האלה מסביב ?" +
+               "\n" + "               ומה זה כל הברדק הזה בחדר שלך ?" +
+               "\n" + "     בו'אנה תן לחזור לים הגדול" +
+               "\n" + "               שם הרבה יותר נעים לי..."+
+               "\n"+ "\n"
+       textView2 = createTextView(text2)
+   }*/
+
+    /* private fun setupButtons() {
+         btn1.setOnClickListener {
+
+             viewNode_1?.setParent(modelNodeGlobal)
+             viewNode_1?.let { it1 -> viewNodes.add(it1) }
+         }
+
+         btn2.setOnClickListener {
+             viewNode_1?.setParent(null)
+             viewNode_2?.setParent(modelNodeGlobal)
+
+         }
+         btn3.setOnClickListener {
+             viewNode_2?.setParent(null)
+
+         }
+
+     }*/
+
+   /* @SuppressLint("ClickableViewAccessibility")
+    private fun setupFab() {
+        fab.setOnClickListener {
 
             fab.setOnLongClickListener {
                 isRecording = videoRecorder.toggleRecordingState()
@@ -226,20 +394,7 @@ class MainActivity : AppCompatActivity() {
                 } else false
             }
         }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }*/
 
 
     /* private fun bubbleView1(): TextView {
@@ -323,7 +478,7 @@ class MainActivity : AppCompatActivity() {
                localPosition = Vector3 (model.radius,model.height,0f)
                localRotation = Quaternion.eulerAngles(Vector3(0f,model.rotationDegrees,0f))
                if (!spaScale){
-                   localScale= Vector3(thisScale,thisScale,thisScale)
+                   localScale= Vector3(modelScale,modelScale,modelScale)
                }
 
             //  setParent(rotatingNode)
@@ -343,7 +498,7 @@ class MainActivity : AppCompatActivity() {
             localPosition = Vector3(box.size.y*messageZ, box.size.y*messageX, box.size.y*messageY)
             localRotation = Quaternion.eulerAngles(Vector3(0f,model.rotationDegrees+90f,0f))
             if (!spaScale){
-                localScale= Vector3(thisScale,thisScale,thisScale)
+                localScale= Vector3(modelScale,modelScale,modelScale)
             }
             setParent(modelNode)
         }
@@ -352,10 +507,10 @@ class MainActivity : AppCompatActivity() {
         val viewNode1=Node().apply {
             renderable=null
             val box = modelNode.renderable?.collisionShape as Box
-            localPosition = Vector3( 0f, box.size.y*bubleX, box.size.y*bubleY)
+            localPosition = Vector3( 0f, box.size.y*verticalDisplacementFactor, box.size.y*horizontalDisplacementFactor)
             localRotation = Quaternion.eulerAngles(Vector3(0f,model.rotationDegrees+90f,0f))
             if (!spaScale){
-                localScale= Vector3(thisScale,thisScale,thisScale)
+                localScale= Vector3(modelScale,modelScale,modelScale)
             }
             setParent(modelNode)
 
--- app/src/main/java/com/sg/newfly20/Models.kt
+++ app/src/main/java/com/sg/newfly20/Models.kt
@@ -11,10 +11,118 @@ sealed class Models {
         override val degreesPerSecond: Float
             get() = 10f
         override val radius: Float
-            get() = 1.5f
+            get() = 0.5f
         override val height: Float
             get() = -0.1f
         override val rotationDegrees: Float
             get() = 180f
     }
+    object Book: Models() {                        //model-202
+        override val degreesPerSecond: Float
+            get() = 10f
+        override val radius: Float
+            get() = 0.5f
+        override val height: Float
+            get() = -0.1f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+    object Wolf: Models() {                        //model-203
+        override val degreesPerSecond: Float
+            get() = 10f
+        override val radius: Float
+            get() = 0.5f
+        override val height: Float
+            get() = 0.2f
+        override val rotationDegrees: Float
+            get() = 180f
+    }
+
+    object Mei_Run: Models() {                        //model-204
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.005f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
+    object BorisEntery: Models() {                        //model-205
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.1f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+    object BorisSammba: Models() {                        //model-205A
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.1f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
+    object BorisHipHop: Models() {                        //model-205B
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.1f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
+    object BorisSwing: Models() {                        //model-205C
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.1f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
+    object DancerGirl: Models() {                        //model-206
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.1f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
+    object Guppie: Models() {                        //model-206
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.1f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
+    object Broaddtail: Models() {                        //model-208
+        override val degreesPerSecond: Float
+            get() = 20f
+        override val radius: Float
+            get() = 0.3f
+        override val height: Float
+            get() = 0.0f
+        override val rotationDegrees: Float
+            get() = 0f
+    }
+
 }
